1fc4931e9b8ad1c2e2fb2e6eb4f495fdb3e452fe
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 16:19:33 +0100

    Update ClassificationDAO.kt

--- app/src/main/java/com/example/cloudbreastcancer/cloudDatabase/ClassificationDAO.kt
+++ app/src/main/java/com/example/cloudbreastcancer/cloudDatabase/ClassificationDAO.kt
@@ -44,22 +44,22 @@ class ClassificationDAO {
         if (line == null) {
             return null
         }
-        val line1vals: ArrayList<String> = Ocl.tokeniseCSV(line)
+        val line1vals: List<String> = Ocl.tokeniseCSV(line)
         var classificationx: Classification? = Classification.ClassificationIndex[line1vals[0]]
         if (classificationx == null) {
             classificationx = Classification.createByPKClassification(line1vals[0])
         }
-        classificationx.one = line1vals[0].toFloat()
-        classificationx.two = line1vals[1].toFloat()
-        classificationx.three = line1vals[2].toFloat()
-        classificationx.four = line1vals[3].toFloat()
-        classificationx.five = line1vals[4].toFloat()
-        classificationx.six = line1vals[5].toFloat()
-        classificationx.seven = line1vals[6].toFloat()
-        classificationx.eight = line1vals[7].toFloat()
-        classificationx.nine = line1vals[8].toFloat()
-        classificationx.result = line1vals[9]
-        classificationx.id = line1vals[10]
+        classificationx.id = line1vals[0]
+        classificationx.age = line1vals[1].toInt()
+        classificationx.bmi = line1vals[2].toFloat()
+        classificationx.glucose = line1vals[3].toFloat()
+        classificationx.insulin = line1vals[4].toFloat()
+        classificationx.homa = line1vals[5].toFloat()
+        classificationx.leptin = line1vals[6].toFloat()
+        classificationx.adiponectin = line1vals[7].toFloat()
+        classificationx.resistin = line1vals[8].toFloat()
+        classificationx.mcp = line1vals[9].toFloat()
+        classificationx.outcome = line1vals[10]
         return classificationx
     }
 
@@ -73,17 +73,17 @@ class ClassificationDAO {
                 if (classificationx == null) {
                     classificationx = Classification.createByPKClassification(id)
                 }
-                classificationx.one = obj.getDouble("one").toFloat()
-                classificationx.two = obj.getDouble("two").toFloat()
-                classificationx.three = obj.getDouble("three").toFloat()
-                classificationx.four = obj.getDouble("four").toFloat()
-                classificationx.five = obj.getDouble("five").toFloat()
-                classificationx.six = obj.getDouble("six").toFloat()
-                classificationx.seven = obj.getDouble("seven").toFloat()
-                classificationx.eight = obj.getDouble("eight").toFloat()
-                classificationx.nine = obj.getDouble("nine").toFloat()
-                classificationx.result = obj.getString("result")
                 classificationx.id = obj.getString("id")
+                classificationx.age = obj.getDouble("age").toInt()
+                classificationx.bmi = obj.getDouble("bmi").toFloat()
+                classificationx.glucose = obj.getDouble("glucose").toFloat()
+                classificationx.insulin = obj.getDouble("insulin").toFloat()
+                classificationx.homa = obj.getDouble("homa").toFloat()
+                classificationx.leptin = obj.getDouble("leptin").toFloat()
+                classificationx.adiponectin = obj.getDouble("adiponectin").toFloat()
+                classificationx.resistin = obj.getDouble("resistin").toFloat()
+                classificationx.mcp = obj.getDouble("mcp").toFloat()
+                classificationx.outcome = obj.getString("outcome")
                 classificationx
             } catch (e: Exception) {
                 null
@@ -95,7 +95,7 @@ class ClassificationDAO {
         if (lines == null) {
             return result
         }
-        val rows: ArrayList<String> = Ocl.parseCSVtable(lines)
+        val rows: List<String> = Ocl.parseCSVtable(lines)
         for (item in rows.indices) {
             val row = rows[item]
             if (row == null || row.trim { it <= ' ' }.isEmpty()) {
@@ -137,17 +137,17 @@ class ClassificationDAO {
         fun writeJSON(x: Classification): JSONObject? {
             val result = JSONObject()
             try {
-                result.put("one", x.one)
-                result.put("two", x.two)
-                result.put("three", x.three)
-                result.put("four", x.four)
-                result.put("five", x.five)
-                result.put("six", x.six)
-                result.put("seven", x.seven)
-                result.put("eight", x.eight)
-                result.put("nine", x.nine)
-                result.put("result", x.result)
                 result.put("id", x.id)
+                result.put("age", x.age)
+                result.put("bmi", x.bmi)
+                result.put("glucose", x.glucose)
+                result.put("insulin", x.insulin)
+                result.put("homa", x.homa)
+                result.put("leptin", x.leptin)
+                result.put("adiponectin", x.adiponectin)
+                result.put("resistin", x.resistin)
+                result.put("mcp", x.mcp)
+                result.put("outcome", x.outcome)
             } catch (e: Exception) {
                 return null
             }
@@ -166,17 +166,17 @@ class ClassificationDAO {
                 if (classificationx == null) {
                     classificationx = Classification.createByPKClassification(id)
                 }
-                classificationx.one = (map["one"]as Long?)!!.toLong().toFloat()
-                classificationx.two = (map["two"] as Long?)!!.toLong().toFloat()
-                classificationx.three = (map["three"]as Long?)!!.toLong().toFloat()
-                classificationx.four = (map["four"] as Long?)!!.toLong().toFloat()
-                classificationx.five = (map["five"]as Long?)!!.toLong().toFloat()
-                classificationx.six = (map["six"] as Long?)!!.toLong().toFloat()
-                classificationx.seven = (map["seven"]as Long?)!!.toLong().toFloat()
-                classificationx.eight = (map["eight"] as Long?)!!.toLong().toFloat()
-                classificationx.nine = (map["nine"]as Long?)!!.toLong().toFloat()
-                classificationx.result = map["result"].toString()
                 classificationx.id = map["id"].toString()
+                classificationx.age = (map["age"] as Long?)!!.toLong().toInt()
+                classificationx.bmi = (map["bmi"]as Long?)!!.toLong().toFloat()
+                classificationx.glucose = (map["glucose"] as Long?)!!.toLong().toFloat()
+                classificationx.insulin = (map["insulin"]as Long?)!!.toLong().toFloat()
+                classificationx.homa = (map["homa"] as Long?)!!.toLong().toFloat()
+                classificationx.leptin = (map["leptin"]as Long?)!!.toLong().toFloat()
+                classificationx.adiponectin = (map["adiponectin"] as Long?)!!.toLong().toFloat()
+                classificationx.resistin = (map["resistin"]as Long?)!!.toLong().toFloat()
+                classificationx.mcp = (map["mcp"]as Long?)!!.toLong().toFloat()
+                classificationx.outcome = map["outcome"].toString()
                 return classificationx
             } catch (e: Exception) {
                 return null
